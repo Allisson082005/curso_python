cd1fb3557fb251c8a494f79c474c5c7790e269e5
Allisson082005 <143648570+Allisson082005@users.noreply.github.com>
2023-09-21 13:39:36 -0600

    Update task.py

--- funciones/ejercicio3/task.py
+++ funciones/ejercicio3/task.py
@@ -4,6 +4,10 @@ def factorial(n):
     n: Es un entero positivo.
     Devuelve el factorial de n.
     """
+     num = 1
+    for i in range(1, n + 1):
+        num *= i
+    return num
     # TODO
 
 
