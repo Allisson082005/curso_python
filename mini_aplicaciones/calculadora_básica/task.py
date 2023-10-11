f4c1971b6431a642af5837051a8de8ed96f2d51f
Allisson082005 <143648570+Allisson082005@users.noreply.github.com>
2023-10-11 13:36:08 -0600

    Update task.py

--- mini_aplicaciones/calculadora_básica/task.py
+++ mini_aplicaciones/calculadora_básica/task.py
@@ -1,20 +1,41 @@
 def suma(a, b):
-# TODO
+    return a + b
 
 def resta(a, b):
-# TODO
+    return a - b
 
 def multiplicacion(a, b):
-# TODO
+    return a * b
 
 def division(a, b):
-# TODO
+    return a / b
 
 def mostrar_menu():
-# TODO
+    print("Menú de opciones:")
+    print("(1) Suma")
+    print("(2) Resta")
+    print("(3) Multiplicación")
+    print("(4) División")
+    print("(5) Salir")
 
-def main():
-# TODO
+def main(menu=None):
+    mostrar_menu()
+   opcion = input("Seleccione una opción: ")
+   a=int(input("Inserte el primer número: "))
+   b=int(input("Inserte el segundo número: "))
+    while opcion != mn[5]:
+        if opcion == "1" :
+           print("resultados", suma(a,b))
+        elif opcion == "2":
+            print("resultados", resta(a, b))
+        elif opcion == "3":
+            print("resultados", multiplicacion(a, b))
+        elif opcion == "4":
+            print("resultados", division(a, b))
+        elif opcion == "5":
+            print("")
+
+
 
 if __name__ == "__main__":
     main()
