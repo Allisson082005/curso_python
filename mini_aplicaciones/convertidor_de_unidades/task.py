9ac742c3ecf7f0399752b19146e03c02e445a9d4
Allisson082005 <143648570+Allisson082005@users.noreply.github.com>
2023-10-11 13:42:20 -0600

    Update task.py

--- mini_aplicaciones/convertidor_de_unidades/task.py
+++ mini_aplicaciones/convertidor_de_unidades/task.py
@@ -1,20 +1,35 @@
 def celsius_a_fahrenheit(celsius):
-# TODO
-
+    return (celsius * 9/5) + 32
 
 def kilometros_a_millas(kilometros):
-# TODO
-
+    return kilometros * 0.621371
 
 def litros_a_galones(litros):
-# TODO
-
+    return litros * 0.264172
 
 def mostrar_menu():
-# TODO
+    print("1. Celsius a Fahrenheit")
+    print("2. Kilómetros a millas")
+    print("3. Litros a galones")
 
 def main():
-# TODO
+    while True:
+        mostrar_menu()
+        opcion = int(input("Seleccione una opción: "))
+        if opcion == 1:
+            celsius = float(input("Ingrese los grados Celsius: "))
+            fahrenheit = celsius_a_fahrenheit(celsius)
+            print(f"{celsius} grados Celsius son equivalentes a {fahrenheit} grados Fahrenheit.")
+        elif opcion == 2:
+            kilometros = float(input("Ingrese los kilómetros: "))
+            millas = kilometros_a_millas(kilometros)
+            print(f"{kilometros} kilómetros son equivalentes a {millas} millas.")
+        elif opcion == 3:
+            litros = float(input("Ingrese los litros: "))
+            galones = litros_a_galones(litros)
+            print(f"{litros} litros son equivalentes a {galones} galones.")
+        else:
+            break
 
 if __name__ == "__main__":
     main()
