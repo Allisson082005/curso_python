36caefdad0108658d4a7b11e97e1f5ab82a39bc4
Allisson082005 <143648570+Allisson082005@users.noreply.github.com>
2023-10-11 13:37:37 -0600

    Update task.py

--- mini_aplicaciones/lista_de_tareas/task.py
+++ mini_aplicaciones/lista_de_tareas/task.py
@@ -1,24 +1,51 @@
 def mostrar_menu():
-# TODO
+    print("(1) Agregar tarea")
+    print("(2) Eliminar tarea")
+    print("(3) Ver tarea")
+    print("(4) Salir")
 
 
 def agregar_tarea(tareas, tarea):
-# TODO
+    tareas.append(tarea)
+    print("Tarea agregada: ", tarea)
 
 
 def eliminar_tarea(tareas, indice):
-# TODO
+    if 0<= indice < len(tareas)
+        tarea_eliminada = tareas.pop(indice)
+        print("Tarea eliminada: ", tarea_eliminada)
+    else:
+        print("Indice invalido, no se puede eliminar tarea.")
+
 
 
 def ver_tareas(tareas):
-# TODO
-
+    print("Tareas en la lista: ")
+    for i
+    tareas
 
 def main():
+    tareas = []
+    opcion=""
+    while opcion != "4":
+        mostrar_menu()
+        opcion = input("EScoja una opción: ")
+        if opcion == "1":
+            a = input("Añade una tarea: ")
+            agregar_tarea(tareas, a)
+        elif opcion == "2":
+            
+            a = input("Elimina una tarea: ")
+            eliminar_tarea(tareas, a)
+        elif opcion == "3":
+            print(ver_tareas())
+        elif opcion == "4":
+            print("")
     """
+    
         Use la lista vacia tareas = [] para agregar, eliminar o ver las tareas
     """
-    tareas = []
+
 # TODO
 
 
